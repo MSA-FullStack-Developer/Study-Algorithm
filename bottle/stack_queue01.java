bd9b02467b3f89d058017399f81fe60b6c9b7bb8
byeounghoon <kimbyeounghoon@naver.com>
2022-03-25 00:07:53 +0900

    [Solve] ✍ 기능개발(Level2)

---
+++ bottle/stack_queue01.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class stack_queue01 {
+    public static void main(String[] args) {
+        int temp[] = new int[100];
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+        int day = 0;
+        int count = 0;
+        
+        for(int i = 0 ; i < progresses.length ; i ++){
+            while(progresses[i] + (day * speeds[i]) < 100){
+                day++;
+            }
+            temp[day]++;
+        }
+        
+        for (int i : temp) {
+            if(i !=0){
+                count++;
+            }
+        }
+        int[] answer = new int[count];
+        count=0;
+        for (int i : temp) {
+             if(i != 0){
+                 answer[count++] = i;
+            }
+        }
+
+    }
+}   
