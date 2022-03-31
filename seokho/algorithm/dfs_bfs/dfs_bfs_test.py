e7306f25e10ccdeaaeabb8f3cb53f8c4fb5acc23
msh1273 <mch1273@naver.com>
2022-03-31 21:08:20 +0900

    [Solve] ✍ 여행경로(level 3)

---
+++ seokho/algorithm/dfs_bfs/dfs_bfs_test.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+visited = [False] * 9
+# dfs(graph, 1, visited)
+bfs(graph, 1, visited)
