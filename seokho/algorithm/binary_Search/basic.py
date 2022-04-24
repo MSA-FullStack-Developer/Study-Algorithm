05f89d1a3689619d475ef538656bf371c4609ce5
msh1273 <mch1273@naver.com>
2022-04-25 00:58:34 +0900

    [Solve] ✍부품찾기 (이진탐색)

--- seokho/algorithm/binary_Search/basic.py
+++ seokho/algorithm/binary_Search/basic.py
@@ -14,10 +14,30 @@ def bin_rec(arr, target, start, end):
         return bin_rec(arr, target, mid+1, end)
 
 
+# 이진 탐색 반복문
+def bin_for(arr, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        # 중간점의 값보다 찾고자 하는 값이 작은 경우
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+
 n, target = list(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-result = bin_rec(arr, target, 0, n-1)
+# result = bin_rec(arr, target, 0, n-1)
+# if result == None:
+#     print("원소가 존재하지 않음")
+# else:
+#     print(result + 1)
+
+result = bin_for(arr, target, 0, n-1)
 if result == None:
     print("원소가 존재하지 않음")
 else:
