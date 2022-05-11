bb6bf9a88560eec2508663400ad82e63cb1091dd
byeounghoon <kimbyeounghoon@naver.com>
2022-05-12 01:05:49 +0900

    [Solve] ✍ BOJ1916 최단비용 구하기

---
+++ bottle/p1916.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class p1916 {
+
+    static final int INF = 987654321;
+    static int n; // 도시
+    static int m; // 버스
+    static List<Node>[] list;
+    static int[] dist;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        list = new ArrayList[n + 1];
+        dist = new int[n + 1];
+        visited = new boolean[n + 1];
+        Arrays.fill(dist, INF);
+
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            list[start].add(new Node(end, weight));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+
+        dijkstra(k);
+        // 배열 및 리스트 초기화 작업
+
+        System.out.println(dist[target]);
+    }
+
+    public static void dijkstra(int start) {
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0));
+        dist[start] = 0;
+
+        while (!queue.isEmpty()) {
+            Node curNode = queue.poll();
+            int cur = curNode.end;
+
+            if (visited[cur]) {
+                continue;
+            }
+            visited[cur] = true;
+
+            for (Node node : list[cur]) {
+                if (dist[node.end] > dist[cur] + node.weight) {
+                    dist[node.end] = dist[cur] + node.weight;
+                    queue.add(new Node(node.end, dist[node.end]));
+                }
+            }
+        }
+    }
+
+    static class Node implements Comparable<Node> {
+        int end, weight;
+
+        public Node(int end, int weight) {
+            this.end = end;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return weight - o.weight;
+        }
+    }
+}
